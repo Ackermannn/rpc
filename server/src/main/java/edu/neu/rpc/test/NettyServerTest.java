baa87e896b61435ef5ee8497b2c0484ea8b046b0
Ackermannn <2421243761@qq.com>
2021-08-23 11:06:23 +0800

    服务端ip自动获取

--- server/src/main/java/edu/neu/rpc/test/NettyServerTest.java
+++ server/src/main/java/edu/neu/rpc/test/NettyServerTest.java
@@ -4,6 +4,10 @@ import edu.neu.rpc.NettyServer;
 import edu.neu.rpc.RpcServer;
 import lombok.extern.slf4j.Slf4j;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * create time: 2021/7/31 下午 3:47
  *
@@ -13,8 +17,14 @@ import lombok.extern.slf4j.Slf4j;
 public class NettyServerTest {
 
     public static void main(String[] args) {
-        String host = "219.216.101.88";
-        int port = 10087;
+        String host = null;
+        try {
+            host = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        log.info("本地ip地址：{}", host);
+        int port = 8765;
 
         RpcServer rs = new NettyServer(host, port);
         rs.start();
